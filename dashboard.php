7cebf2ba160ae9a66a061e904140e31292dd0afa
Surjya Bhowmick <bhowmickneeds@gmail.com>
2026-03-02 02:05:49 +0600

    added dashboard

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['userID'])) {
+    header("Location: login.html");
+    exit;
+}
+
+echo "Welcome, " . $_SESSION['username'];
+?>
+
+<!-- Add the content of the dashboard -->
